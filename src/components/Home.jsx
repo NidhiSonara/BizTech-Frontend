713e0af4fbec4c171169f3fd5e10cad95d8ddda0
NidhiSonara <nidhisonara@gmail.com>
2022-11-25 16:16:50 +0530

    Delete Employee

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,52 @@ import Table from 'react-bootstrap/Table';
 import Loader from "../container/Loader";
 import { Link } from "react-router-dom";
 import { toast } from 'react-toastify';
+import { Button } from 'react-bootstrap';
+import swal from 'sweetalert';
 function Home() {
     const [loading, setLoading] = useState(true);
     const [employeeData, setEmployeeData] = useState();
     useEffect(() => {
+        getEmployeeList()
+    }, []);
+
+    const getEmployeeList = () => {
         api.get(`/list`).then((res) => {
-            console.log("🚀 ~ file: Home.jsx ~ line 9 ~ api.get ~ res", res)
             if (res.status === 200) {
                 setEmployeeData(res.data.data);
                 setLoading(false);
             }
         }).catch((err) => {
             toast.error(err.message);
-        });;
-    }, []);
+        });
+    }
+
+    const handleDelete = (employee) => {
+        swal({
+            title: "Are you sure you want to delete this Employee?",
+            text: "Once deleted, you will not be able to recover this!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                api.delete(`/delete?_id=${employee?._id}`).then((res) => {
+                    if (res.status === 200) {
+                        // toast.success(res.data.message);
+                        swal("Poof! Employee has been deleted!", {
+                            icon: "success",
+                        });
+                        getEmployeeList();
+                    } else {
+                        toast.error(res.data.message);
+                    }
+                });
+
+            } else {
+                swal("Employee is safe!");
+            }
+        });
+    }
 
     return (
         <div className='p-3'>
@@ -53,7 +85,7 @@ function Home() {
                                     <td><img className='img-fluid rounded-circle' src={employee?.image} alt="profile" /></td>
                                     <td>
                                         <Link to={`/edit/${employee._id}`} className='btn btn-primary my-3'>Edit</Link>
-                                        <Link to="/add" className='btn btn-danger my-3'>Delete</Link>
+                                        <Button className='btn btn-danger my-3' onClick={() => handleDelete(employee)}>Delete</Button>
                                     </td>
                                 </tr>
                             ))}
